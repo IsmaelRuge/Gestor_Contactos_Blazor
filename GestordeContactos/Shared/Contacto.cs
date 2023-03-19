67c7591e344d0fa91c3d1e12b71970163b9cf6f8
IsmaelRuge <ismaelruge@gmail.com>
2023-03-19 10:16:25 -0500

    Gestor de Contatos Blazor

---
+++ GestordeContactos/Shared/Contacto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestordeContactos.Shared
+{
+    public class Contacto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Celular { get; set; }
+        public string Dirección { get; set; }
+
+        public string NombreCompleto { get { return Apellido + ", " + Nombre; } }
+    }
+}
